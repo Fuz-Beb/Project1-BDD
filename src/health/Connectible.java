d3b48faa2ce7700b01b3b4b508ba2b7d6848ca9e
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-09-08 15:34:37 -0600

    Connectible update + Delete objects

--- src/health/Connectible.java
+++ src/health/Connectible.java
@@ -1,8 +1,6 @@
 package health;
 
-import java.util.HashMap;
 import javax.json.stream.JsonGenerator;
-import org.xml.sax.Attributes;
 
 // Travail fait par :
 // Bobet Pierrick - 17 131 792
@@ -11,468 +9,204 @@ import org.xml.sax.Attributes;
 public class Connectible
 {
     // Attributes
-    private HashMap<Integer, Atrium> atriumTab;
-    private HashMap<Integer, Ventricle> ventricleTab;
-    private HashMap<Integer, Artery> arteryTab;
-    private HashMap<Integer, Vein> veinTab;
-    private HashMap<Integer, Capillaries> capillariesTab;
-    private HashMap<Integer, Nose> noseTab;
-    private HashMap<Integer, AirConnectible> airConnectibleTab;
-    private HashMap<Integer, Alveoli> alveoliTab;
-    private HashMap<Integer, DigestiveTract> digestiveTractTab;
-    private HashMap<Integer, StomachTract> stomachTractTab;
-    private HashMap<Integer, DuodenumTract> duodenumTractTab;
-    private HashMap<Integer, RectumTract> rectumTractTab;
-    private HashMap<Integer, BiDuct> biductTab;
-    private HashMap<Integer, Duct> ductTab;
-    private HashMap<Integer, DuctOverflowableJunction> ductOverflowableJunctionTab;
-    private HashMap<Integer, DeversingDuct> deversingDuctTab;
-    private HashMap<Integer, InnerGallbladder> innerGallbladderTab;
-    private HashMap<Integer, SalivaryDuct> salivaryDuctTab;
+    private String type;
+    private int id;
+    private String name;
+    private Double volume;
+    private Double length;
+    private Double startRadius;
+    private Double endRadius;
 
     // Comfort Constructor
-    public Connectible(Attributes attrs)
+
+    /**
+     * Constructeur de confort avec les attributs communs
+     * 
+     * @param type
+     * @param id
+     * @param name
+     */
+    public Connectible(String type, int id, String name)
     {
-        atriumTab = new HashMap<Integer, Atrium>();
-        ventricleTab = new HashMap<Integer, Ventricle>();
-        arteryTab = new HashMap<Integer, Artery>();
-        veinTab = new HashMap<Integer, Vein>();
-        capillariesTab = new HashMap<Integer, Capillaries>();
-        noseTab = new HashMap<Integer, Nose>();
-        airConnectibleTab = new HashMap<Integer, AirConnectible>();
-        alveoliTab = new HashMap<Integer, Alveoli>();
-        digestiveTractTab = new HashMap<Integer, DigestiveTract>();
-        stomachTractTab = new HashMap<Integer, StomachTract>();
-        duodenumTractTab = new HashMap<Integer, DuodenumTract>();
-        rectumTractTab = new HashMap<Integer, RectumTract>();
-        biductTab = new HashMap<Integer, BiDuct>();
-        ductTab = new HashMap<Integer, Duct>();
-        ductOverflowableJunctionTab = new HashMap<Integer, DuctOverflowableJunction>();
-        deversingDuctTab = new HashMap<Integer, DeversingDuct>();
-        innerGallbladderTab = new HashMap<Integer, InnerGallbladder>();
-        salivaryDuctTab = new HashMap<Integer, SalivaryDuct>();
+        this.type = type;
+        this.id = id;
+        this.name = name;
     }
 
     /**
-     * @return the atriumTab
+     * Constructeur de confort avec les attributs communs et spéciaux
+     * 
+     * @param type
+     * @param id
+     * @param name
+     * @param volume
+     * @param length
+     * @param startRadius
+     * @param endRadius
      */
-    public HashMap<Integer, Atrium> getAtriumTab()
+    public Connectible(String type, int id, String name, Double volume, Double length, Double startRadius,
+            Double endRadius)
     {
-        return atriumTab;
+        this(type, id, name);
+        this.volume = volume;
+        this.length = length;
+        this.startRadius = startRadius;
+        this.endRadius = endRadius;
+    }
+
+    // Getters / Setters
+
+    /**
+     * @return the type
+     */
+    public String getType()
+    {
+        return type;
     }
 
     /**
-     * @param atriumTab
-     *            the atriumTab to set
+     * @param type
+     *            the type to set
      */
-    public void setAtriumTab(HashMap<Integer, Atrium> atriumTab)
+    public void setType(String type)
     {
-        this.atriumTab = atriumTab;
+        this.type = type;
     }
 
     /**
-     * @return the ventricleTab
+     * @return the id
      */
-    public HashMap<Integer, Ventricle> getVentricleTab()
+    public int getId()
     {
-        return ventricleTab;
+        return id;
     }
 
     /**
-     * @param ventricleTab
-     *            the ventricleTab to set
+     * @param id
+     *            the id to set
      */
-    public void setVentricleTab(HashMap<Integer, Ventricle> ventricleTab)
+    public void setId(int id)
     {
-        this.ventricleTab = ventricleTab;
+        this.id = id;
     }
 
     /**
-     * @return the arteryTab
+     * @return the name
      */
-    public HashMap<Integer, Artery> getArteryTab()
+    public String getName()
     {
-        return arteryTab;
+        return name;
     }
 
     /**
-     * @param arteryTab
-     *            the arteryTab to set
+     * @param name
+     *            the name to set
      */
-    public void setArteryTab(HashMap<Integer, Artery> arteryTab)
+    public void setName(String name)
     {
-        this.arteryTab = arteryTab;
+        this.name = name;
     }
 
     /**
-     * @return the veinTab
+     * @return the volume
      */
-    public HashMap<Integer, Vein> getVeinTab()
+    public Double getVolume()
     {
-        return veinTab;
+        return volume;
     }
 
     /**
-     * @param veinTab
-     *            the veinTab to set
+     * @param volume
+     *            the volume to set
      */
-    public void setVeinTab(HashMap<Integer, Vein> veinTab)
+    public void setVolume(Double volume)
     {
-        this.veinTab = veinTab;
+        this.volume = volume;
     }
 
     /**
-     * @return the capillariesTab
+     * @return the length
      */
-    public HashMap<Integer, Capillaries> getCapillariesTab()
+    public Double getLength()
     {
-        return capillariesTab;
+        return length;
     }
 
     /**
-     * @param capillariesTab
-     *            the capillariesTab to set
+     * @param length
+     *            the length to set
      */
-    public void setCapillariesTab(HashMap<Integer, Capillaries> capillariesTab)
+    public void setLength(Double length)
     {
-        this.capillariesTab = capillariesTab;
+        this.length = length;
     }
 
     /**
-     * @return the noseTab
+     * @return the startRadius
      */
-    public HashMap<Integer, Nose> getNoseTab()
+    public Double getStartRadius()
     {
-        return noseTab;
+        return startRadius;
     }
 
     /**
-     * @param noseTab
-     *            the noseTab to set
+     * @param startRadius
+     *            the startRadius to set
      */
-    public void setNoseTab(HashMap<Integer, Nose> noseTab)
+    public void setStartRadius(Double startRadius)
     {
-        this.noseTab = noseTab;
+        this.startRadius = startRadius;
     }
 
     /**
-     * @return the airConnectibleTab
+     * @return the endRadius
      */
-    public HashMap<Integer, AirConnectible> getAirConnectibleTab()
+    public Double getEndRadius()
     {
-        return airConnectibleTab;
+        return endRadius;
     }
 
     /**
-     * @param airConnectibleTab
-     *            the airConnectibleTab to set
+     * @param endRadius
+     *            the endRadius to set
      */
-    public void setAirConnectibleTab(HashMap<Integer, AirConnectible> airConnectibleTab)
+    public void setEndRadius(Double endRadius)
     {
-        this.airConnectibleTab = airConnectibleTab;
-    }
-
-    /**
-     * @return the alveoliTab
-     */
-    public HashMap<Integer, Alveoli> getAlveoliTab()
-    {
-        return alveoliTab;
-    }
-
-    /**
-     * @param alveoliTab
-     *            the alveoliTab to set
-     */
-    public void setAlveoliTab(HashMap<Integer, Alveoli> alveoliTab)
-    {
-        this.alveoliTab = alveoliTab;
-    }
-
-    /**
-     * @return the digestiveTractTab
-     */
-    public HashMap<Integer, DigestiveTract> getDigestiveTractTab()
-    {
-        return digestiveTractTab;
-    }
-
-    /**
-     * @param digestiveTractTab
-     *            the digestiveTractTab to set
-     */
-    public void setDigestiveTractTab(HashMap<Integer, DigestiveTract> digestiveTractTab)
-    {
-        this.digestiveTractTab = digestiveTractTab;
-    }
-
-    /**
-     * @return the stomachTractTab
-     */
-    public HashMap<Integer, StomachTract> getStomachTractTab()
-    {
-        return stomachTractTab;
-    }
-
-    /**
-     * @param stomachTractTab
-     *            the stomachTractTab to set
-     */
-    public void setStomachTractTab(HashMap<Integer, StomachTract> stomachTractTab)
-    {
-        this.stomachTractTab = stomachTractTab;
-    }
-
-    /**
-     * @return the duodenumTractTab
-     */
-    public HashMap<Integer, DuodenumTract> getDuodenumTractTab()
-    {
-        return duodenumTractTab;
-    }
-
-    /**
-     * @param duodenumTractTab
-     *            the duodenumTractTab to set
-     */
-    public void setDuodenumTractTab(HashMap<Integer, DuodenumTract> duodenumTractTab)
-    {
-        this.duodenumTractTab = duodenumTractTab;
-    }
-
-    /**
-     * @return the rectumTractTab
-     */
-    public HashMap<Integer, RectumTract> getRectumTractTab()
-    {
-        return rectumTractTab;
-    }
-
-    /**
-     * @param rectumTractTab
-     *            the rectumTractTab to set
-     */
-    public void setRectumTractTab(HashMap<Integer, RectumTract> rectumTractTab)
-    {
-        this.rectumTractTab = rectumTractTab;
-    }
-
-    /**
-     * @return the biductTab
-     */
-    public HashMap<Integer, BiDuct> getBiductTab()
-    {
-        return biductTab;
-    }
-
-    /**
-     * @param biductTab
-     *            the biductTab to set
-     */
-    public void setBiductTab(HashMap<Integer, BiDuct> biductTab)
-    {
-        this.biductTab = biductTab;
-    }
-
-    /**
-     * @return the ductTab
-     */
-    public HashMap<Integer, Duct> getDuctTab()
-    {
-        return ductTab;
-    }
-
-    /**
-     * @param ductTab
-     *            the ductTab to set
-     */
-    public void setDuctTab(HashMap<Integer, Duct> ductTab)
-    {
-        this.ductTab = ductTab;
-    }
-
-    /**
-     * @return the ductOverflowableJunctionTab
-     */
-    public HashMap<Integer, DuctOverflowableJunction> getDuctOverflowableJunctionTab()
-    {
-        return ductOverflowableJunctionTab;
-    }
-
-    /**
-     * @param ductOverflowableJunctionTab
-     *            the ductOverflowableJunctionTab to set
-     */
-    public void setDuctOverflowableJunctionTab(HashMap<Integer, DuctOverflowableJunction> ductOverflowableJunctionTab)
-    {
-        this.ductOverflowableJunctionTab = ductOverflowableJunctionTab;
-    }
-
-    /**
-     * @return the deversingDuctTab
-     */
-    public HashMap<Integer, DeversingDuct> getDeversingDuctTab()
-    {
-        return deversingDuctTab;
-    }
-
-    /**
-     * @param deversingDuctTab
-     *            the deversingDuctTab to set
-     */
-    public void setDeversingDuctTab(HashMap<Integer, DeversingDuct> deversingDuctTab)
-    {
-        this.deversingDuctTab = deversingDuctTab;
-    }
-
-    /**
-     * @return the innerGallbladderTab
-     */
-    public HashMap<Integer, InnerGallbladder> getInnerGallbladderTab()
-    {
-        return innerGallbladderTab;
-    }
-
-    /**
-     * @param innerGallbladderTab
-     *            the innerGallbladderTab to set
-     */
-    public void setInnerGallbladderTab(HashMap<Integer, InnerGallbladder> innerGallbladderTab)
-    {
-        this.innerGallbladderTab = innerGallbladderTab;
-    }
-
-    /**
-     * @return the salivaryDuctTab
-     */
-    public HashMap<Integer, SalivaryDuct> getSalivaryDuctTab()
-    {
-        return salivaryDuctTab;
-    }
-
-    /**
-     * @param salivaryDuctTab
-     *            the salivaryDuctTab to set
-     */
-    public void setSalivaryDuctTab(HashMap<Integer, SalivaryDuct> salivaryDuctTab)
-    {
-        this.salivaryDuctTab = salivaryDuctTab;
+        this.endRadius = endRadius;
     }
 
     // Methods
 
-    public void addAtrium(Atrium atrium)
-    {
-        atriumTab.put(atriumTab.size(), atrium);
-    }
-
-    public void addVentricle(Ventricle ventricle)
-    {
-        ventricleTab.put(ventricleTab.size(), ventricle);
-    }
-
-    public void addArtery(Artery artery)
-    {
-        arteryTab.put(arteryTab.size(), artery);
-    }
-
-    public void addVein(Vein vein)
-    {
-        veinTab.put(veinTab.size(), vein);
-    }
-
-    public void addCapillaries(Capillaries capillaries)
-    {
-        capillariesTab.put(capillariesTab.size(), capillaries);
-    }
-
-    public void addNose(Nose nose)
-    {
-        noseTab.put(noseTab.size(), nose);
-    }
-
-    public void addAirConnectible(AirConnectible airConnectible)
-    {
-        airConnectibleTab.put(airConnectibleTab.size(), airConnectible);
-    }
-
-    public void addAlveoli(Alveoli alveoli)
-    {
-        alveoliTab.put(alveoliTab.size(), alveoli);
-    }
-
-    public void addDigestiveTract(DigestiveTract digestiveTract)
-    {
-        digestiveTractTab.put(digestiveTractTab.size(), digestiveTract);
-    }
-
-    public void addStomachTract(StomachTract stomachTract)
-    {
-        stomachTractTab.put(stomachTractTab.size(), stomachTract);
-    }
-
-    public void addDuodenumTract(DuodenumTract duodenumTract)
-    {
-        duodenumTractTab.put(duodenumTractTab.size(), duodenumTract);
-    }
-
-    public void addRectumTract(RectumTract rectumTract)
-    {
-        rectumTractTab.put(rectumTractTab.size(), rectumTract);
-    }
-
-    public void addBiduct(BiDuct biDuct)
-    {
-        biductTab.put(biductTab.size(), biDuct);
-    }
-
-    public void addDuct(Duct duct)
-    {
-        ductTab.put(ductTab.size(), duct);
-    }
-
-    public void addDuctOverflowableJunction(DuctOverflowableJunction ductOverflowableJunction)
-    {
-        ductOverflowableJunctionTab.put(ductOverflowableJunctionTab.size(), ductOverflowableJunction);
-    }
-
-    public void addDeversingDuct(DeversingDuct deversingDuct)
-    {
-        deversingDuctTab.put(deversingDuctTab.size(), deversingDuct);
-    }
-
-    public void addInnerGallbladder(InnerGallbladder innerGallbladder)
-    {
-        innerGallbladderTab.put(innerGallbladderTab.size(), innerGallbladder);
-    }
-
-    public void addSalivaryDuct(SalivaryDuct salivaryDuct)
-    {
-        salivaryDuctTab.put(salivaryDuctTab.size(), salivaryDuct);
-    }
-    
     /**
      * Convertit l'objet actuel en JSON
+     * 
      * @param jsonGenerator
-     */    
-    public void toJSON(JsonGenerator jsonGenerator) {
+     */
+    public void toJSON(JsonGenerator jsonGenerator)
+    {
         // Ecrit l'objet actuel dans le générateur JSON
-        generator.writeStartObject();
-        generator.write("id", this.getId());
-        generator.write("type", this.getType());
-        generator.write("name", this.getName());
-        if (this.getVolume() != null) {
-            generator.write("volume", this.getVolume());
+        jsonGenerator.writeStartObject();
+        jsonGenerator.write("id", this.getId());
+        jsonGenerator.write("type", this.getType());
+        jsonGenerator.write("name", this.getName());
+
+        if (this.getVolume() != null)
+        {
+            jsonGenerator.write("volume", this.getVolume());
         }
-        if (this.getStartRadius() != null) {
-            generator.write("startRadius", this.getStartRadius());
+        if (this.getStartRadius() != null)
+        {
+            jsonGenerator.write("startRadius", this.getStartRadius());
         }
-        if (this.getEndRadius() != null) {
-            generator.write("endRadius", this.getEndRadius());
+        if (this.getEndRadius() != null)
+        {
+            jsonGenerator.write("endRadius", this.getEndRadius());
         }
-        if (this.getLength() != null) {
-            generator.write("length", this.getLength());
+        if (this.getLength() != null)
+        {
+            jsonGenerator.write("length", this.getLength());
         }
-        generator.writeEnd();
+
+        jsonGenerator.writeEnd();
     }
 }
